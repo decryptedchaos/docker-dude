1eb725ac2a7d38bb8435ca903bed5913cc06f154
decryptedchaos <nixgod@gmail.com>
2019-08-28 16:35:40 -0500

    Update dude.sh
    Override for testing

--- container/dude.sh
+++ container/dude.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
-export DISPLAY=:1
-rm -f /tmp/.X1-lock
-Xvfb :1 -screen 0 800x600x16 &
+#export DISPLAY=:1
+#rm -f /tmp/.X1-lock
+#Xvfb :1 -screen 0 800x600x16 &
 # Prevents Wine from prompting for Mono and Gecko
-export WINEDLLOVERRIDES="mscoree,mshtml="
-wine 'z:\dude\dude.exe' --server
+#export WINEDLLOVERRIDES="mscoree,mshtml="
+#wine 'z:\dude\dude.exe' --server
+sh
