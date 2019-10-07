176a3ba4176ae77747ecc3326d7cb78f2ef075b9
decryptedchaos <nixgod@gmail.com>
2019-10-06 23:26:47 -0500

    Update dude.sh

--- container/dude.sh
+++ container/dude.sh
@@ -2,8 +2,8 @@
 
 #export DISPLAY=:1
 #rm -f /tmp/.X1-lock
-#Xvfb :1 -screen 0 800x600x16 &
+Xvfb :1 -screen 0 800x600x16 &
 # Prevents Wine from prompting for Mono and Gecko
 #export WINEDLLOVERRIDES="mscoree,mshtml="
-#wine 'z:\dude\dude.exe' --server
-sh
+wine 'z:\dude\dude.exe' --server
+#sh
